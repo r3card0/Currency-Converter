4451026ea717d9ce6e110274f5480dfebc6a02b3
r3card0 <ricardo.rdz@pm.me>
2021-12-06 09:58:18 -0600

    Challenge:calculate dollar price from mexican pesos

--- conversor.py
+++ conversor.py
@@ -1,15 +1,18 @@
-#crear una variable que se llame "pesos" l10
+#crear una variable que se llame "pesos" 
 #crear variable "valor_dolar", con el valor de 3875. Este
-#valor es la equivalencia de dolar en pesos colombianos l12
+#valor es la equivalencia de dolar en pesos colombianos 
 #hacer la operacion en base a los dolares que tenemos
-#y se crea la variable dolares, que contendrá la operacion l13
+#y se crea la variable dolares, que contendrá la operacion 
 #Reducir las decimales en el resultado
 #usar el comando round, que significa redondear, la variable mas la
-#cantidad de decimales que se requiera agregar l14
-#convertir los dolares a un valor de texto, para imprimirlo en consola l15
-pesos = input("¿Cuántos pesos colombianos tienes?:  ")
+#cantidad de decimales que se requiera agregar 
+#convertir los dolares a un valor de texto, para imprimirlo en consola 
+## challenge: hacer que el programa realice el calculo en la moneda nacional
+## precio del dolar al 6 de Dic, 2021 : 21.20
+## **pesos colombianos** pesos = input("¿Cuántos pesos colombianos tienes?:  ")
+pesos = input("¿Cuántos pesos mexicanos tienes?:  ")
 pesos = float(pesos)
-valor_dolar = 3875 
+valor_dolar = 21.20 
 dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares =str(dolares)
