3ecd4752df5ed0e236a3bab7def24945b4be8bb3
r3card0 <ricardo.rdz@pm.me>
2022-09-26 20:53:40 -0500

    Added function menu

--- currency_converter.py
+++ currency_converter.py
@@ -5,8 +5,20 @@ def description():
     """
     return program_description
 
+# menu
+def menu():
+    menu_= int(input("""Welcome to the currency converter 😃💰
+    1. Pesos colombianos
+    2. Pesos mexicanos
+    3. Pesos argentinos
+    Please! Choice an option:  """))
+    
+    return menu_
+
+
 def run():
     print(description())
+    print(menu())
 
 if __name__ == "__main__":
     run()
