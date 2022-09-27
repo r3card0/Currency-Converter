326352d0d48b936a485fdbd8eb7856b8882b4ac1
r3card0 <ricardo.rdz@pm.me>
2022-09-26 21:16:30 -0500

    added preliminar code

--- menu_2.py
+++ menu_2.py
@@ -1,9 +1,23 @@
 def description():
-    pass
+    program_description= """
+    This program shows options to convert dollars
+    """
+    return program_description
+
+# menu
+def menu_2():
+    menu_= int(input("""Welcome to the currency converter 😃💰
+    1. Pesos colombianos
+    2. Pesos mexicanos
+    3. Pesos argentinos
+    Please! Choice an option:  """))
+    
+    return menu_
+
 
 def run():
-    pass
-
+    print(description())
+    print(menu_2())
 
 if __name__ == "__main__":
     run()
