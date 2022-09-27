b9643123479acd6b3789f7d12ce3d1b46885055e
r3card0 <ricardo.rdz@pm.me>
2022-09-26 21:22:24 -0500

    runs the first menu

--- menu_1.py
+++ menu_1.py
@@ -1,9 +1,23 @@
 def description():
-    pass
+    program_description= """
+    This program shows options to convert to dollar to pesos or to pesos to dollars
+    """
+    return program_description
+
+# menu
+def menu_1():
+    menu_= int(input("""Welcome to the currency converter 😃💰
+    1. Dollars to pesos
+    2. Pesos to dollars
+    
+    Please! Choose an option:  """))
+    
+    return menu_
+
 
 def run():
-    pass
-
+    print(description())
+    print(menu_1())
 
 if __name__ == "__main__":
     run()
