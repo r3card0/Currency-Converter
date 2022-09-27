1cac81721ce9decadb6c941e3138cd7a05b921a0
r3card0 <ricardo.rdz@pm.me>
2022-09-26 20:40:04 -0500

    Added description of the program

---
+++ currency_converter.py
@@ -0,0 +1,12 @@
+def description():
+    program_description= """
+    This program makes currency convertion from pesos 
+    to dollars
+    """
+    return program_description
+
+def run():
+    print(description())
+
+if __name__ == "__main__":
+    run()
