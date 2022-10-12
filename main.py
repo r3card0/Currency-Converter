6df9f034b1abb487d08b7343396edfa5d82e915f
r3card0 <ricardo.rdz@pm.me>
2022-10-11 21:45:09 -0500

    added structure for main program

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from menu_2 import menu_2
+from main_menu import main_menu
+
+def algo():
+    menu = main_menu()
+    try:
+        if menu == 1:
+            return 'run function dollars to pesos'
+        elif menu == 2:
+            return 'run function pesos to dollars'
+        else:
+            return 'Choose a correct option'
+    except ValueError:
+        return 'Please, enter an integer number'
+
+def run():
+    print(algo())
+
+if __name__ == '__main__':
+    run()
