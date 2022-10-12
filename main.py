7d115b05549cf2cec25945e0a4e9bb098165ad57
r3card0 <ricardo.rdz@pm.me>
2022-10-11 23:21:18 -0500

    added pesos to dollar function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from main_menu import main_menu
 from dollars_to_pesos import dollars_to_pesos
+from pesos_to_dollars import pesos_to_dollars
 
 def algo():
     menu = main_menu()
@@ -7,7 +8,7 @@ def algo():
         if menu == 1:
             return dollars_to_pesos()
         elif menu == 2:
-            return 'run function pesos to dollars'
+            return pesos_to_dollars()
         else:
             return 'Choose a correct option'
     except ValueError:
