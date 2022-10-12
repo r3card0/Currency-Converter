14e08994331d208f50a80de22319cb773e0e0ea9
r3card0 <ricardo.rdz@pm.me>
2022-10-11 23:01:47 -0500

    started to build conversion

---
+++ dollars_to_pesos.py
@@ -0,0 +1,18 @@
+def dollars_to_pesos():
+    try:
+        dollars = float(input('Enter dollars amount: '))
+        # dollars * pesos = new_pesos_amount
+        pesos = float(20.15)
+        output = dollars * pesos
+        return output 
+    
+    except ValueError:
+        return 'Please enter a numerical value'
+
+
+def run():
+    print(dollars_to_pesos())
+
+
+if __name__ == '__main__':
+    run()
