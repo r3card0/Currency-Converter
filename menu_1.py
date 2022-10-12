7937c1e930211aa8a3153b819c5e346443ddd47e
r3card0 <ricardo.rdz@pm.me>
2022-10-11 21:07:03 -0500

    select option function

--- menu_1.py
+++ menu_1.py
@@ -6,18 +6,27 @@ def description():
 
 # menu
 def menu_1():
-    menu_= int(input("""Welcome to the currency converter 😃💰
+    menu: int = int(input("""Welcome to the currency converter 😃💰
     1. Dollars to pesos
     2. Pesos to dollars
     
     Please! Choose an option:  """))
     
-    return menu_
+    return menu
+
+def select_option():
+    menu = menu_1()
+    if menu == 1:
+        print('Convert dollars to pesos')
+    elif menu == 2:
+        print('Convert pesos to dollars')
+    else:
+        print('Choose a correct option')
 
 
 def run():
     print(description())
-    print(menu_1())
+    print(select_option())
 
 if __name__ == "__main__":
     run()
